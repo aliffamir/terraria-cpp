0cbe3d2b55912b0f16795cb475704e0ac85d255c
Aliff Amir <aliff6406@gmail.com>
2025-05-12 14:48:27 +0100

    feat: update position of passed in frame on key press

--- game/include/gameLogic.h
+++ game/include/gameLogic.h
@@ -1,8 +1,10 @@
 #ifndef gameLogic_h
 #define gameLogic_h
 
+#include "Input.h"
+
 bool initGameplay();
-bool gameplayFrame(double deltaTime, int width, int height);
+bool gameplayFrame(double deltaTime, int width, int height, const Input& input, FrameOutput& frame);
 void closeGameLogic();
 
 #endif
--- game/src/gameLogic.cpp
+++ game/src/gameLogic.cpp
@@ -1,3 +1,5 @@
+#include "FrameOutput.h"
+#include "Input.h"
 #include <iostream>
 
 struct GameData
@@ -19,7 +21,37 @@ bool initGameplay()
 };
 
 // Called every frame
-bool gameplayFrame(double deltaTime, int width, int height) {};
+bool gameplayFrame(double deltaTime, int width, int height, const Input& input, FrameOutput& output)
+{
+    // Handle movement
+    if (input.keys[SDL_SCANCODE_UP].held)
+        data.positionY -= int(200 * deltaTime);
+    if (input.keys[SDL_SCANCODE_DOWN].held)
+        data.positionY += int(200 * deltaTime);
+    if (input.keys[SDL_SCANCODE_LEFT].held)
+        data.positionX -= int(200 * deltaTime);
+    if (input.keys[SDL_SCANCODE_RIGHT].held)
+        data.positionX += int(200 * deltaTime);
+
+    // Clamp to screen bounds
+    data.positionX = std::clamp(data.positionX, 0, width - 50);
+    data.positionY = std::clamp(data.positionY, 0, height - 50);
+
+    // FPS tracking (optional)
+    data.fpsCounter++;
+    data.timer += deltaTime;
+    if (data.timer >= 1.0)
+    {
+        std::cout << "FPS: " << data.fpsCounter << "\n";
+        data.timer -= 1.0;
+        data.fpsCounter = 0;
+    }
+
+    // Output a single red rectangle
+    output.rects.push_back({data.positionX, data.positionY, 50, 50, 255, 0, 0, 255});
+
+    return true;
+};
 
 // game cleanup goes here
 void closeGameLogic() {};
