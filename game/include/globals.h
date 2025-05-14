859761157ea1bed5fe57d09be867a9a8fcdd04f4
Aliff Amir <aliff6406@gmail.com>
2025-05-14 23:08:46 +0100

    feat: change SPRITE_SCALE constant to 1.0

--- game/include/globals.h
+++ game/include/globals.h
@@ -5,7 +5,7 @@ namespace Globals
 {
 constexpr int SCREEN_WIDTH = 800;
 constexpr int SCREEN_HEIGHT = 400;
-constexpr float SPRITE_SCALE = 2.0f;
+constexpr float SPRITE_SCALE = 1.0f;
 } // namespace Globals
 
 enum Direction
