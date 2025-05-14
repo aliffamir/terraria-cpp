ba8bd455e5f3af06767a292943bf10ce17663ca1
Aliff Amir <aliff6406@gmail.com>
2025-05-14 12:38:13 +0100

    feat: remove include and forward declare structs

--- game/include/graphics.h
+++ game/include/graphics.h
@@ -1,7 +1,11 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
-#include <SDL2/SDL.h>
+// Forward declare these structs so we don't have to include SDL2 here
+struct SDL_Window;
+struct SDL_Renderer;
+struct SDL_Texture;
+struct SDL_Rect;
 
 class Graphics
 {
