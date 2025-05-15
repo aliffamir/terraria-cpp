6316de56c1d46240fa41d4071747d6e557dde570
Aliff Amir <aliff6406@gmail.com>
2025-05-15 13:07:19 +0100

    feat: add Vector2 struct

--- game/include/globals.h
+++ game/include/globals.h
@@ -8,6 +8,25 @@ constexpr int SCREEN_HEIGHT = 400;
 constexpr float SPRITE_SCALE = 1.0f;
 } // namespace Globals
 
+struct Vector2
+{
+    int x;
+    int y;
+
+    Vector2() : x{0}, y{0}
+    {
+    }
+
+    Vector2(int x, int y) : x{x}, y{y}
+    {
+    }
+
+    Vector2 zero()
+    {
+        return Vector2(0, 0);
+    }
+};
+
 enum Direction
 {
     LEFT,
