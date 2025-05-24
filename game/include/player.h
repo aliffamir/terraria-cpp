7b853260612944f780165b283f9d4e1a1c48eebc
Aliff Amir <aliff6406@gmail.com>
2025-05-24 12:53:10 +0100

    add player methods to move and stop moving

--- game/include/player.h
+++ game/include/player.h
@@ -2,6 +2,7 @@
 #define PLAYER_H
 
 #include "animatedsprite.h"
+#include "globals.h"
 #include "graphics.h"
 
 class Player : public AnimatedSprite
@@ -14,6 +15,31 @@ class Player : public AnimatedSprite
 
     virtual void animationDone(std::string currentAnimation);
     virtual void setupAnimations();
+
+    /*
+    void moveLeft
+    Moves the player left by -dx
+    */
+    void moveLeft();
+
+    /*
+    void moveRight
+    Moves the player right by +dx
+    */
+    void moveRight();
+
+    /*
+    void stopMoving
+    Stops moving the player
+    */
+    void stopMoving();
+
+  private:
+    // change in x and y position
+    float _dx;
+    float _dy;
+
+    Direction _facing;
 };
 
 #endif
--- game/src/player.cpp
+++ game/src/player.cpp
@@ -1,6 +1,11 @@
 #include "player.h"
 #include <string>
 
+namespace PlayerConstants
+{
+const float WALK_SPEED = 0.2f;
+}
+
 Player::Player(Graphics& graphics, float x, float y)
     : AnimatedSprite{graphics, "../assets/sprites/walk.png", 0, 64, 64, 64, x, y, 100}
 {
@@ -11,6 +16,8 @@ Player::Player(Graphics& graphics, float x, float y)
 
 void Player::setupAnimations()
 {
+    addAnimation(2, 0, 64, "IdleLeft", 64, 64, Vector2{0, 0});
+    addAnimation(2, 0, 128, "IdleRight", 64, 64, Vector2{0, 0});
     addAnimation(9, 0, 64, "WalkLeft", 64, 64, Vector2{0, 0});
     addAnimation(9, 0, 192, "WalkRight", 64, 64, Vector2{0, 0});
 }
@@ -19,8 +26,29 @@ void Player::animationDone(std::string currentAnimation)
 {
 }
 
+void Player::moveLeft()
+{
+    _dx = -PlayerConstants::WALK_SPEED;
+    playAnimation("WalkLeft");
+    _facing = LEFT;
+}
+
+void Player::moveRight()
+{
+    _dx = PlayerConstants::WALK_SPEED;
+    playAnimation("WalkRight");
+    _facing = RIGHT;
+}
+
+void Player::stopMoving()
+{
+    _dx = 0.0f;
+    playAnimation(_facing == LEFT ? "IdleLeft" : "IdleRight");
+}
+
 void Player::update(float elapsedTime)
 {
+    _x += _dx * elapsedTime;
     AnimatedSprite::update(elapsedTime);
 }
 
